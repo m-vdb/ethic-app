b92b07a6bf8faae52dbf7604b1510a460e5172e5
m-vdb <mvergerdelbove@work4labs.com>
2015-09-05 11:58:12 -0700

    testserver.js to serve static files

---
+++ testserver.js
@@ -0,0 +1,13 @@
+var http = require('http'),
+    static = require('node-static');
+const PORT = 8080;
+
+// we want to server everything
+var file = new static.Server('.');
+
+http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        // serve files
+        file.serve(request, response);
+    }).resume();
+}).listen(PORT);
