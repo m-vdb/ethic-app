4bd6bbfd8a46b6c05ae100b653023ae288d74075
m-vdb <mvergerdelbove@work4labs.com>
2015-10-30 20:35:37 -0700

    setup chai in setup.js

--- test/setup.js
+++ test/setup.js
@@ -4,3 +4,11 @@ window.Backbone = require('backbone');
 require('marionette');
 require('backbone-validation');
 require('../ethic/utils/backbone.inheritance.coffee');
+
+
+window.chai = require("chai");
+window.expect = chai.expect;
+var sinonChai = require("sinon-chai");
+var chaiFuzzy = require("chai-fuzzy");
+chai.use(sinonChai);
+chai.use(chaiFuzzy);
